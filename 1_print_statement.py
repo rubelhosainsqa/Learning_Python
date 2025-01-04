1a35ef0438d4c98adb7babc3e53400c9ccf03997
Rubel Hosain <rh.riyon.bd@gmail.com>
2025-01-04 19:16:07 +0600

    initial commit

---
+++ 1_print_statement.py
@@ -0,0 +1,9 @@
+print("I am Rubel")
+print('I am SQA engineer')
+print(9)
+
+# end="" is used for print multiple line in one line
+
+print('I am a Rubel.', end=" ")
+print('I working as a SQA engineer at atB jobs bangladesh.', end=" ")
+print('I have 2 years job experience.')
